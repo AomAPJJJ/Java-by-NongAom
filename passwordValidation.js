2b9dd008ea8b024e468767dc022dbcc964dc0707
Panipak Jongsirivanich <panipak.jong@kmutt.ac.th>
2023-12-14 02:30:54 +0700

    Add files via upload

---
+++ passwordValidation.js
@@ -0,0 +1,13 @@
+// 65130500050 Panipak Jongsirivanich
+function isPasswordValid(password) {
+  if(password === null || password === undefined ||password.length < 8 || !password.match(/[a-z]/) || !password.match(/[A-Z]/) || !password.match(/[0-9]/) || !password.match(/[@#$%^&\*!]/)){
+      return false
+  }else{
+  return true
+}
+
+}
+
+module.exports = isPasswordValid
+
+
